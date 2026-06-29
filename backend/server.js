5893592dc55c091fc170174574f1d63661511168
Rohith Gaddam <gaddamrohith7396@gmail.com>
2026-06-29 18:03:04 +0530

    Fixed MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 // MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/socialmedia")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
